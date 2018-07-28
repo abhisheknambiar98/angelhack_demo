dc6fb70b14a85d72ec24b5405cc16ff0ebf93de3
Abhishek Nambiar <abhisheknambiar98@dev.abhishek>
2018-07-28 22:35:32 +0530

    back-changed

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const mongoose =require('mongoose')
 //const cors= require('cors')
 app.set('view engine','ejs')
 
-app.use(static('static'))
+//app.use(static('static'))
 
+app.get('/',(requestAnimationFrame,res)=>{
+    res.render('main.ejs')
+})
 app.listen(3000,()=>{
     console.log("Logged into port 3000")
 })
